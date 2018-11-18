75288f5993dd0df17bd410575d8d51b22c951122
ColumbusTech <ColumbusTech@yandex.ru>
2018-11-18 12:08:08 +0300

    TGA

--- tga.hpp
+++ tga.hpp
@@ -54,7 +54,8 @@ namespace tga
 	{
 		Monochrome,
 		RGB,
-		RGBA
+		RGBA,
+		Undefined
 	};
 
 	class TGA
@@ -76,146 +77,15 @@ namespace tga
 		void RGBACompressed(uint8_t* InBuffer, uint8_t* OutBuffer, size_t Size);
 		void MonochromeCompressed(uint8_t* InBuffer, uint8_t* OutBuffer, size_t Size);
 	public:
-		TGA() : Data(nullptr), Size(0) {}
+		TGA() : Data(nullptr), Size(0), Width(0), Height(0), Format(ImageFormat::Undefined) {}
 
-		bool Load(const std::string& Filename)
-		{
-			std::ifstream File(Filename, std::ios::binary);
-			if (!File.is_open()) return false;
+		uint8_t* GetData() const { return Data; }
+		uint64_t GetSize() const { return Size; }
+		uint32_t GetWidth() const { return Width; }
+		uint32_t GetHeight() const { return Height; }
+		ImageFormat GetFormat() const { return Format; }
 
-			Header Head;
-			size_t FileSize = 0;
-
-			File.seekg(0, std::ios_base::end);
-			FileSize = File.tellg();
-			File.seekg(0, std::ios_base::beg);
-
-			File.read((char*)&Head.idlen,                sizeof(Head.idlen));
-			File.read((char*)&Head.color_map_type,       sizeof(Head.color_map_type));
-			File.read((char*)&Head.image_type,           sizeof(Head.image_type));
-			File.read((char*)&Head.color_map_origin,     sizeof(Head.color_map_origin));
-			File.read((char*)&Head.color_map_length,     sizeof(Head.color_map_length));
-			File.read((char*)&Head.color_map_entry_size, sizeof(Head.color_map_entry_size));
-			File.read((char*)&Head.x_origin,             sizeof(Head.x_origin));
-			File.read((char*)&Head.y_origin,             sizeof(Head.y_origin));
-			File.read((char*)&Head.width,                sizeof(Head.width));
-			File.read((char*)&Head.height,               sizeof(Head.height));
-			File.read((char*)&Head.bits,                 sizeof(Head.bits));
-			File.read((char*)&Head.image_descriptor,     sizeof(Head.image_descriptor));
-				
-			uint8_t* Descriptor = new uint8_t[Head.image_descriptor];
-			File.read((char*)Descriptor, Head.image_descriptor);
-			
-			size_t ColorMapElementSize = Head.color_map_entry_size / 8;
-			size_t ColorMapSize = Head.color_map_length * ColorMapElementSize;
-			uint8_t* ColorMap = new uint8_t[ColorMapSize];
-
-			if (Head.color_map_type == 1)
-			{
-				File.read((char*)ColorMap, ColorMapSize);
-			}
-
-			size_t PixelSize = Head.color_map_length == 0 ? (Head.bits / 8) : ColorMapElementSize;
-			size_t DataSize = FileSize - sizeof(Header);
-			size_t ImageSize = Head.width * Head.height * PixelSize;
-
-			uint8_t* Buffer = new uint8_t[DataSize];
-			File.read((char*)Buffer, DataSize);
-
-			Data = new uint8_t[ImageSize];
-			memset(Data, 0, ImageSize);
-
-			switch (Head.image_type)
-			{
-				case 0: break; // No Image
-				case 1: // Uncompressed paletted
-				{
-					if (Head.bits == 8)
-					{
-						switch (PixelSize)
-						{
-						case 3: RGBPaletted((uint8_t*)Buffer, ColorMap, Data, Head.width * Head.height);  break;
-						case 4: RGBAPaletted((uint8_t*)Buffer, ColorMap, Data, Head.width * Head.height); break;
-						}
-					}
-					else if (Head.bits == 16)
-					{
-						switch (PixelSize)
-						{
-						case 3: RGBPaletted((uint16_t*)Buffer, ColorMap, Data, Head.width * Head.height);  break;
-						case 4: RGBAPaletted((uint16_t*)Buffer, ColorMap, Data, Head.width * Head.height); break;
-						}
-					}
-
-					break;
-				}
-				case 2: // Uncompressed TrueColor
-				{
-					if (Head.bits = 24 || Head.bits == 32)
-					{
-						std::copy(&Buffer[0], &Buffer[ImageSize], &Data[0]);
-
-						for (size_t i = 0; i < ImageSize; i += PixelSize)
-						{
-							std::swap(Data[i], Data[i + 2]);
-						}
-					}
-
-					break;
-				}
-
-				case 3: // Uncompressed Monochrome
-				{
-					if (Head.bits == 8)
-					{
-						std::copy(&Buffer[0], &Buffer[ImageSize], &Data[0]);
-					}
-
-					break;
-				}
-
-				case 9: break; // Compressed paletted TODO
-				case 10: // Compressed TrueColor
-				{
-					if (Head.bits == 24)
-					{
-						RGBCompressed(Buffer, Data, Head.width * Head.height);
-					}
-					else if (Head.bits == 32)
-					{
-						RGBACompressed(Buffer, Data, Head.width * Head.height);
-					}
-
-					break;
-				}
-
-				case 11: // Compressed Monocrhome
-				{
-					if (Head.bits == 8)
-					{
-						MonochromeCompressed(Buffer, Data, Head.width * Head.height);
-					}
-
-					break;
-				}
-			}
-
-			switch (PixelSize)
-			{
-			case 1: Format = ImageFormat::Monochrome; break;
-			case 3: Format = ImageFormat::RGB;        break;
-			case 4: Format = ImageFormat::RGBA;       break;
-			}
-
-			Width = Head.width;
-			Height = Head.height;
-			Size = Head.width * Head.height * PixelSize;
-
-			delete[] ColorMap;
-			delete[] Descriptor;
-
-			return true;
-		}
+		bool Load(const std::string& Filename);
 
 		~TGA()
 		{
@@ -357,6 +227,145 @@ namespace tga
 				i += pixelcount;
 			}
 		}
+	}
+
+	bool TGA::Load(const std::string& Filename)
+	{
+		std::ifstream File(Filename, std::ios::binary);
+		if (!File.is_open()) return false;
+
+		Header Head;
+		size_t FileSize = 0;
+
+		File.seekg(0, std::ios_base::end);
+		FileSize = File.tellg();
+		File.seekg(0, std::ios_base::beg);
+
+		File.read((char*)&Head.idlen,                sizeof(Head.idlen));
+		File.read((char*)&Head.color_map_type,       sizeof(Head.color_map_type));
+		File.read((char*)&Head.image_type,           sizeof(Head.image_type));
+		File.read((char*)&Head.color_map_origin,     sizeof(Head.color_map_origin));
+		File.read((char*)&Head.color_map_length,     sizeof(Head.color_map_length));
+		File.read((char*)&Head.color_map_entry_size, sizeof(Head.color_map_entry_size));
+		File.read((char*)&Head.x_origin,             sizeof(Head.x_origin));
+		File.read((char*)&Head.y_origin,             sizeof(Head.y_origin));
+		File.read((char*)&Head.width,                sizeof(Head.width));
+		File.read((char*)&Head.height,               sizeof(Head.height));
+		File.read((char*)&Head.bits,                 sizeof(Head.bits));
+		File.read((char*)&Head.image_descriptor,     sizeof(Head.image_descriptor));
+
+		uint8_t* Descriptor = new uint8_t[Head.image_descriptor];
+		File.read((char*)Descriptor, Head.image_descriptor);
+
+		size_t ColorMapElementSize = Head.color_map_entry_size / 8;
+		size_t ColorMapSize = Head.color_map_length * ColorMapElementSize;
+		uint8_t* ColorMap = new uint8_t[ColorMapSize];
+
+		if (Head.color_map_type == 1)
+		{
+			File.read((char*)ColorMap, ColorMapSize);
+		}
+
+		size_t PixelSize = Head.color_map_length == 0 ? (Head.bits / 8) : ColorMapElementSize;
+		size_t DataSize = FileSize - sizeof(Header);
+		size_t ImageSize = Head.width * Head.height * PixelSize;
+
+		uint8_t* Buffer = new uint8_t[DataSize];
+		File.read((char*)Buffer, DataSize);
+
+		Data = new uint8_t[ImageSize];
+		memset(Data, 0, ImageSize);
+
+		switch (Head.image_type)
+		{
+		case 0: break; // No Image
+		case 1: // Uncompressed paletted
+		{
+			if (Head.bits == 8)
+			{
+				switch (PixelSize)
+				{
+				case 3: RGBPaletted((uint8_t*)Buffer, ColorMap, Data, Head.width * Head.height);  break;
+				case 4: RGBAPaletted((uint8_t*)Buffer, ColorMap, Data, Head.width * Head.height); break;
+				}
+			}
+			else if (Head.bits == 16)
+			{
+				switch (PixelSize)
+				{
+				case 3: RGBPaletted((uint16_t*)Buffer, ColorMap, Data, Head.width * Head.height);  break;
+				case 4: RGBAPaletted((uint16_t*)Buffer, ColorMap, Data, Head.width * Head.height); break;
+				}
+			}
+
+			break;
+		}
+		case 2: // Uncompressed TrueColor
+		{
+			if (Head.bits = 24 || Head.bits == 32)
+			{
+				std::copy(&Buffer[0], &Buffer[ImageSize], &Data[0]);
+
+				for (size_t i = 0; i < ImageSize; i += PixelSize)
+				{
+					std::swap(Data[i], Data[i + 2]);
+				}
+			}
+
+			break;
+		}
+
+		case 3: // Uncompressed Monochrome
+		{
+			if (Head.bits == 8)
+			{
+				std::copy(&Buffer[0], &Buffer[ImageSize], &Data[0]);
+			}
+
+			break;
+		}
+
+		case 9: break; // Compressed paletted TODO
+		case 10: // Compressed TrueColor
+		{
+			if (Head.bits == 24)
+			{
+				RGBCompressed(Buffer, Data, Head.width * Head.height);
+			}
+			else if (Head.bits == 32)
+			{
+				RGBACompressed(Buffer, Data, Head.width * Head.height);
+			}
+
+			break;
+		}
+
+		case 11: // Compressed Monocrhome
+		{
+			if (Head.bits == 8)
+			{
+				MonochromeCompressed(Buffer, Data, Head.width * Head.height);
+			}
+
+			break;
+		}
+		}
+
+		switch (PixelSize)
+		{
+		case 1: Format = ImageFormat::Monochrome; break;
+		case 3: Format = ImageFormat::RGB;        break;
+		case 4: Format = ImageFormat::RGBA;       break;
+		}
+
+		Width = Head.width;
+		Height = Head.height;
+		Size = Head.width * Head.height * PixelSize;
+
+		delete[] ColorMap;
+		delete[] Descriptor;
+
+		return true;
 	}
 
 #undef READPIXEL8
